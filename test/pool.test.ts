1f9e6e8c01fc9b7166bce3df6229ded2d48623b0
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-02-07 01:35:31 +0100

    test pool with long running task

--- test/pool.test.ts
+++ test/pool.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "@esm-bundle/chai"
-import { HelloWorldApiType } from "./threads/hello-world.worker"
 import { EsThread, EsThreadPool } from "../src/controller";
+import { HelloWorldApiType } from "./threads/hello-world.worker"
+import { LongRunningApiType } from "./threads/long-running.worker";
 
 describe("Run some basic pool tests", () => {
     it("Launch a pool", async () => {
@@ -9,6 +10,21 @@ describe("Run some basic pool tests", () => {
             {type: "module"})), {size: 1});
 
         expect(await pool.queue(worker => worker.methods.helloWorld())).to.be.eq("Hello World!");
+
+        await pool.terminate();
+    });
+
+    it("Launch a pool with long running task", async () => {
+        const pool = await EsThreadPool.Spawn<LongRunningApiType>(() => EsThread.Spawn(
+            new Worker(new URL("threads/long-running.worker.ts", import.meta.url),
+            {type: "module"})), {size: 1});
+
+        const result = pool.queue(worker => worker.methods.takesTime());
+        
+        expect((pool as any).threads[0].numQueuedJobs).to.be.eq(1);
+        await pool.settled();
+        expect((pool as any).threads[0].numQueuedJobs).to.be.eq(0);
+        expect(await result).to.be.eq("Hello World!");
 
         await pool.terminate();
     });
